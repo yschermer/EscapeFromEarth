a59c615ba412acc59ed566fd882d0415ee8dbf35
yschermer <yschermer@gmail.com>
2016-01-17 09:34:05 +0100

    Added time and distance

--- Assets/Scripts/Generate.cs
+++ Assets/Scripts/Generate.cs
@@ -8,12 +8,9 @@ public class Generate : MonoBehaviour {
     public GameObject asteroid3;
     public GameObject sunlight;
     public GameObject meteor;
-	int distance;
 
 	// Use this for initialization
 	void Start () {
-		distance = 0;
-
 		InvokeRepeating ("CreateAsteroid", 0.1f, Random.Range(0.7f, 1.2f));
 		InvokeRepeating ("CreateSunlight", 0.1f, Random.Range(0.5f, 1f));
         InvokeRepeating("CreateMeteor", 5f, 5f);
@@ -23,16 +20,9 @@ public class Generate : MonoBehaviour {
 	void Update () {
 		
 	}
-		
-	void OnGUI()
-	{
-		GUI.color = Color.white;
-		GUILayout.Label ("Distance: " + distance.ToString());
-	}
 
 	void CreateAsteroid()
 	{
-		distance++;
 		Instantiate (asteroid1);
         Instantiate(asteroid2);
         Instantiate(asteroid3);
---
+++ Assets/Scripts/Time.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Diagnostics;
+
+public class Time : MonoBehaviour
+{
+    Text textComponent;
+    Stopwatch timer;
+
+    // Use this for initialization
+    void Start()
+    {
+        textComponent = GetComponent<Text>();
+        timer = new Stopwatch();
+        timer.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textComponent.text = string.Format("Time: {0} seconds", timer.Elapsed.Seconds);
+    }
+}
