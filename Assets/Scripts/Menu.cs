3a96fde7e913b44157c3bf4464ef001bb8086fe1
yschermer <yschermer@gmail.com>
2016-01-18 17:04:50 +0100

    Added logo, modified score

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+    public Canvas menu;
+    public Button play;
+    public Button exit;
+
+	// Use this for initialization
+	void Start () {
+        menu = menu.GetComponent<Canvas>();
+        play = play.GetComponent<Button>();
+        exit = exit.GetComponent<Button>();
+
+        menu.enabled = true;
+    }
+	
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        menu.enabled = false;
+        Application.LoadLevel("Play");
+    }
+}
